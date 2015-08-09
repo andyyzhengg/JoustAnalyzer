66b3b4ff1317cf8c1f96fae5b37a10575ec32171
acheng6845 <acheng6845@gmail.com>
2015-08-09 02:28:32 -0400

    WHEN DID ECLIPSE GO RETARD

--- src/JoustView/CopyPasta.java
+++ src/JoustView/CopyPasta.java
@@ -4,8 +4,8 @@ import java.text.DecimalFormat;
 import org.json.*;
 
 public class CopyPasta {
-	public void printMyJobIsDone(double result) {
+	public static void printMyJobIsDone(double result) {
 		DecimalFormat f = new DecimalFormat("##.00");
-		System.out.println(f.format(result*100));
+		System.out.println("Probability of victory!: " + f.format(result*100) + "%");
 	}
 }
